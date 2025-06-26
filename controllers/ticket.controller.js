ba263e926d04e740f7f7979fc5ba09cd22424c37
DalineSeritsane <dalinelerato@gmail.com>
2025-06-26 04:49:48 +0200

    routes added

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -1,4 +1,4 @@
-import Ticket from '../models/Ticket.js';
+import Ticket from '../models/ticket.js';
 import { getAIResponse } from '../utils/aiAssistant.js';
 export const submitTicket = async (req, res) => {
   const { message } = req.body;
